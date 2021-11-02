6293bd48d2d95264b255777bf257921f6b450f2a
boredom2 <christoph.brosdau@3q.video>
2021-11-02 13:32:25 +0100

    adding TikTok to external Platforms

--- src/enums/externalplatforms.php
+++ src/enums/externalplatforms.php
@@ -5,6 +5,7 @@ class externalplatforms extends \nexxomnia\internals\enum{
 
 	const FACEBOOK='facebook';
 	const INSTAGRAM='instagram';
+	const TIKTOK='tiktok';
 	const TWITTER='twitter';
 	const VIMEO='vimeo';
 	const YOUTUBE='youtube';
