18e5e4c3aae25a65caf59eb8f75679e2869c02eb
boredom2 <christoph.brosdau@3q.video>
2022-05-10 16:55:49 +0800

    Fix DeviceTypes

--- src/enums/devicetypes.php
+++ src/enums/devicetypes.php
@@ -3,10 +3,12 @@ namespace nexxomnia\enums;
 
 class devicetypes extends \nexxomnia\internals\enum{
 
-	const PC='video';
+	const PC='pc';
 	const MOBILE='mobile';
 	const TABLET='tablet';
 	const TV='tv';
+	const WATCH='watch';
 	const SMART='smart';
+	const CAR='car';
 
 }
