73fcf7e66265291f1e170ad46aad42472b3633c3
boredom2 <christoph.brosdau@3q.video>
2022-05-10 17:47:58 +0800

    Fix Gateways

--- src/enums/gateways.php
+++ src/enums/gateways.php
@@ -6,12 +6,14 @@ class gateways extends \nexxomnia\internals\enum{
 	const PC='desktop';
 	const MOBILE='mobile';
 	const TABLET='tablet';
+	const CAR='car';
 	const AMP='amp';
 	const FBIA='fbia';
 	const IOS='ios';
 	const ANDROID='android';
 	const SMARTTV='smarttv';
 	const ANDROIDTV='androidtv';
+	const ANDROIDCAR='androidcar';
 	const podcast='podcast';
 
 	public static function getMediaGateways():array{
