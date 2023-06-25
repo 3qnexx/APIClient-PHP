e07da19fd2dd4e9b80673ffb2f2f570178527d0f
boredom2 <christoph.brosdau@3q.video>
2023-06-26 05:43:01 +0800

    adding missing Gateways/Device Types

--- src/enums/gateways.php
+++ src/enums/gateways.php
@@ -12,9 +12,18 @@ class gateways extends \nexxomnia\internals\enum{
 	const IOS='ios';
 	const ANDROID='android';
 	const SMARTTV='smarttv';
+	const CHROMECAST='chromecast';
 	const ANDROIDTV='androidtv';
-	const ANDROIDCAR='androidcar';
-	const podcast='podcast';
+	const ANDROIDAUTO='androidauto';
+	const CARPLAY='carplay';
+	const PODCAST='podcast';
+	const WATCH='watch';
+	const XBOX='xbox';
+	const PLAYSTATION='playstation';
+	const WINDOWS='windows';
+	const CHROMEOS='chromeos';
+	const MACOS='macos';
+	const VR='vr';
 
 	public static function getMediaGateways():array{
 		return([self::PC,self::MOBILE,self::SMARTTV,self::CAR]);
